c648fab3149ccd5de3a9e7c957454ca3ec859038
Annu Elizabeth Abraham <nueliza@gmail.com>
2019-12-05 17:39:23 -0500

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <header id="home">
+                <div className="banner">
+                    <div className="banner-text">
+                        <h1>Hello! I'm Annu Abraham.</h1>
+                        <h3></h3>
+                    </div>
+                </div>
+            </header>
+        )
+    }
+}
+
+export default Header;
