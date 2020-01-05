ca3290ca83bd34daa56b167c31cf07f90e9d968a
Annu Elizabeth Abraham <nueliza@gmail.com>
2020-01-04 19:31:18 -0500

    about page set up

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from "./components/header";
+import About from "./components/about";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <About />
       </div>
     )
   }
--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { faChevronCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import profile from "../assets/images/picture.png";
 
 class Header extends Component{
     render(){
         return(
             <header id="home">
                 <div className="banner">
+                    <div className="banner-img">
+                        <img src={profile} alt="That's me"></img>
+                    </div>
                     <div className="banner-text">
+                        
                         <h1>Hello! I'm Annu Abraham.</h1>
-                        <h3></h3>
+                        <h3>I'm from Rockville, MD. Something about me!</h3>
                     </div>
                 </div>
+                
+                <p className="scrolldown">
+                    <a className="smoothscroll" href="#about"><FontAwesomeIcon icon={faChevronCircleDown} color="white" /></a>
+                </p>
             </header>
         )
     }
