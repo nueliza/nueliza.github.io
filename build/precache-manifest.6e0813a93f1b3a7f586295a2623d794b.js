c60cc65b901a433edeaaf2a61012941a43964b77
Annu Elizabeth Abraham <nueliza@gmail.com>
2020-01-06 11:57:43 -0500

    build

---
+++ build/precache-manifest.6e0813a93f1b3a7f586295a2623d794b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfcde52c62765ffc5ec969254be1d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2cd9036e132b38a9da7",
+    "url": "/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "6cd62bb7f38cbac1d786",
+    "url": "/static/css/main.304e016b.chunk.css"
+  },
+  {
+    "revision": "b2cd9036e132b38a9da7",
+    "url": "/static/js/2.64f14336.chunk.js"
+  },
+  {
+    "revision": "6cd62bb7f38cbac1d786",
+    "url": "/static/js/main.e0d7d6bb.chunk.js"
+  },
+  {
+    "revision": "e97ceebcb8bbb5abd2af",
+    "url": "/static/js/runtime-main.a5c812ab.js"
+  },
+  {
+    "revision": "bf472238161058594beffb054b8e7958",
+    "url": "/static/media/bg.bf472238.jpg"
+  },
+  {
+    "revision": "6a158866fb814df74bcb347c41127f59",
+    "url": "/static/media/picture.6a158866.png"
+  }
+]);
