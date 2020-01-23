aba2730928ff76c756e0710d545667ffc56ff611
Annu Elizabeth Abraham <nueliza@gmail.com>
2020-01-23 15:34:42 -0500

    About page new layout

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import resume from "../assets/resume.pdf";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 class About extends Component{
     render(){
@@ -9,18 +9,35 @@ class About extends Component{
             <section id="about">
                 
                 <h2> Contact details</h2>
-                Annu Elizabeth Abraham<br/>
-                15955 Frderick Rd, Apt #2251<br/>
-                Rockville, Maryland 20855<br/>
-                860-995-0878<br/>
-                nueliza@gmail.com 
 
-                <span className="download-resume">
+                <div className="about-item-container">
+                    <div className="about-item">
+                        <FontAwesomeIcon icon={faPhone} size="3x" /><br/>
+                        <div className="about-item-content">
+                            860-995-0878
+                        </div>
+                    </div>
+                    <div className="about-item">
+                        <FontAwesomeIcon icon={faEnvelope} size="3x" /><br/>
+                        <div className="about-item-content">
+                            nueliza@gmail.com
+                        </div>
+                    </div>
+                    <div className="about-item">
+                        <FontAwesomeIcon icon={faMapMarkerAlt} size="3x" /><br/>
+                        <div className="about-item-content">
+                            15955 Frderick Rd, Apt #2251<br/>
+                            Rockville, Maryland 20855<br/>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="download-resume">
                     <a href={resume} target= "/">
                         <FontAwesomeIcon icon={faDownload} size="1x" />
                         &nbsp;&nbsp;Download resume
                     </a>
-                </span>
+                </div>
                 <br />
             </section>
         )
