94e5b6108f2d90328aed22e90d1cd5c91043ca47
Annu Elizabeth Abraham <nueliza@gmail.com>
2020-02-04 13:02:26 -0500

    open graph tags try with react helmet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Helmet} from 'react-helmet';
 
 import Header from "./components/header";
 import About from "./components/about";
 import Projects from "./components/projects";
+
+import previewImage from "../src/assets/images/searchResultImage.png";
 
 class App extends Component {
   
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <title>Annu Elizabeth</title>
+          <meta name="description" content="Annu Abraham Portfolio"/>
+          <meta name="image" content={previewImage} />
+        </Helmet>
         <Header />
         <About />
         <Projects />
